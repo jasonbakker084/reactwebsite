37c9f9f1c3a267467b1f1fe8b85769530a228805
Jason Bakker <jasonbakker084@gmail.com>
2020-06-17 11:21:06 +0200

    finished reactwebsite

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Contact = () => {
+    return (
+        <div>
+            <h2>Contact Us</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur non, ut voluptate tenetur reiciendis nam voluptates! Cupiditate perferendis, aliquid et illo velit quaerat ad dolore esse voluptatibus similique hic laboriosam?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, sint dolores? Ex quod illo eaque esse beatae obcaecati nobis hic tenetur, soluta iusto quam vel quae necessitatibus nesciunt dolore voluptate.</p>
+        </div>
+    )
+}
